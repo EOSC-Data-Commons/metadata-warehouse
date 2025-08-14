121de17493622b252cef2110c9843e94b57590e2
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-08-14 15:37:13 +0200

    feat (transform): query raw XML from postgres

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -1,11 +1,28 @@
 import pgsql
 #import psycopg2
 from opensearchpy import OpenSearch
-from tasks import celery_app, add
+#from tasks import celery_app, add
+import os
 
+#print(os.environ)
 
-#with pgsql.Connection(("postgres", 5432), "admin", "test", tls = False) as db:
-    #print(db)
+user = os.environ['POSTGRES_USER']
+pw = os.environ['POSTGRES_PASSWORD']
+
+with pgsql.Connection(('postgres', 5432), user, pw, tls = False) as db:
+    print(db)
+
+    with db.prepare("""
+    SELECT xpath('/oai:record', info, '{{oai, http://www.openarchives.org/OAI/2.0/},{datacite, http://datacite.org/schema/kernel-4}}') AS root
+FROM raw;
+    """) as docs:
+
+        all_rows = docs()
+        #print(len(all_rows))
+
+        for doc in all_rows():
+            print(doc)
+
 
 
 #conn = psycopg2.connect(database = 'postgres', user= 'admin', password= 'test', host= 'postgres', port = 5432)
@@ -23,9 +40,11 @@ client = OpenSearch(
 print(f'opensearch client info: {client.info()}')
 '''
 
+'''
 print('putting some tasks into queue')
 
 for n in range(10):
     result = add.delay(4, 70)
     print(result)
     print(f'added {n} task')
+'''
