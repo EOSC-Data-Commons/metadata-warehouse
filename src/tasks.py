6f960febbe42bdeb2b47bc0f39acf28622dbe21c
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-08-18 15:24:44 +0200

    refactor (docker): clean up compose and move to override

--- src/tasks.py
+++ src/tasks.py
@@ -5,11 +5,11 @@ from opensearchpy.helpers import bulk
 import xmltodict
 import normalize_datacite_json
 
+
 def flush_bulk(client, batch):
     success, failed = bulk(client, batch)
     print(success, failed)
 
-print('celery')
 
 celery_app = Celery('tasks')
 celery_app.task_serializer = 'json'
@@ -23,24 +23,11 @@ client = OpenSearch(
 )
 
 @celery_app.task
-def add(x, y):
-    print('sleeping')
-    time.sleep(10)
-    print('slept')
-    #raise Exception('bad')
-    return x + y
-
-@celery_app.task
 def transform_batch(batch: list[str]):
-    print('batch registered')
-
-    #print(xmltodict.parse(batch[0], process_namespaces=True))
-
-    print('processing batch')
-
-    #transform to JSON and normalize
-
-    converted = list(map(lambda el: normalize_datacite_json.normalize_datacite_json(xmltodict.parse(el, process_namespaces=True)['http://www.openarchives.org/OAI/2.0/:metadata']['http://datacite.org/schema/kernel-4:resource']), batch))
+    # transform to JSON and normalize
+    converted = list(map(lambda el: normalize_datacite_json.normalize_datacite_json(
+        xmltodict.parse(el, process_namespaces=True)['http://www.openarchives.org/OAI/2.0/:metadata'][
+            'http://datacite.org/schema/kernel-4:resource']), batch))
 
     os_eles = list(map(lambda el: {"_op_type": "index", "_index": "test_datacite", "_source": el}, converted))
 
--- src/transform.py
+++ src/transform.py
@@ -1,11 +1,8 @@
 import pgsql
 #import psycopg2
-from opensearchpy import OpenSearch
-from tasks import celery_app, add, transform_batch
+from tasks import transform_batch
 import os
 from fastapi import FastAPI
-
-#print(os.environ)
 
 user = os.environ['POSTGRES_USER']
 pw = os.environ['POSTGRES_PASSWORD']
@@ -31,52 +28,4 @@ async def root():
 
     transform_batch.delay(batch)
 
-    return {"put batch:": f"{len(batch)}"}
-
-'''
-batch = []
-
-with pgsql.Connection(('postgres', 5432), user, pw, tls = False) as db:
-    #print(db)
-
-    with db.prepare("""
-    SELECT (xpath('/oai:record/oai:metadata', info, '{{oai, http://www.openarchives.org/OAI/2.0/},{datacite, http://datacite.org/schema/kernel-4}}'))[1] AS root
-FROM raw;
-    """) as docs:
-
-        all_rows = docs()
-        #print(len(all_rows))
-
-        for doc in all_rows():
-            batch.append(doc.root)
-
-
-res = transform_batch.delay(batch)
-#print(res)
-'''
-
-
-
-#conn = psycopg2.connect(database = 'postgres', user= 'admin', password= 'test', host= 'postgres', port = 5432)
-#cursor = conn.cursor()
-#print(cursor)
-
-'''
-host = 'opensearch'
-client = OpenSearch(
-    hosts=[{'host': host, 'port': 9200}],
-    http_auth=None,
-    use_ssl=False
-)
-
-print(f'opensearch client info: {client.info()}')
-'''
-
-'''
-print('putting some tasks into queue')
-
-for n in range(10):
-    result = add.delay(4, 70)
-    print(result)
-    print(f'added {n} task')
-'''
+    return {'put into batch:': f'{len(batch)}'}
