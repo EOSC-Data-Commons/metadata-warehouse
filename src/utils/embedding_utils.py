ce839e0db0f6a80dda6c53b080f4c0196f3d713c
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-08-26 08:53:26 +0200

    docs (embedding utils): add doc strings

--- src/utils/embedding_utils.py
+++ src/utils/embedding_utils.py
@@ -3,28 +3,57 @@ from typing import Any, NamedTuple
 from fastembed import TextEmbedding
 
 class SourceWithEmbeddingText(NamedTuple):
-    src: dict[str, Any] # 0
-    textToEmbed: str # 1
-    file: Path # 2
+    src: dict[str, Any] # 0, source document
+    textToEmbed: str # 1, text to be embedded
+    file: Path # 2, name of the original source file
 
 def get_embedding_text_from_fields(source: dict[str, Any]) -> str:
+    """
+    Given a source document, extracts the text fields to be embedded and joins them to a single string.
+
+    :param source: the source document.
+    :return: the string to be embedded.
+    """
     return ' '.join(extract_fields_from_source(source, 'titles', 'title') + extract_fields_from_source(source, 'subjects',
                                                                                        'subject') + extract_fields_from_source(
         source, 'descriptions', 'description'))
 
 
 def extract_fields_from_source(source: dict[str, Any], field_name: str, subfield_name: str) -> list[str]:
+    """
+    Given a source document, extracts the subfields for a given field name.
+
+    :param source: the source document.
+    :param field_name: name of the field.
+    :param subfield_name: name of the subfield.
+    :return: the subfield's values.
+    """
     # check if field exists
     if field_name in source:
         return list(map(lambda title: title[subfield_name], source[field_name]))
     else:
         return []
 
-def add_embeddings_to_source(batch: list[SourceWithEmbeddingText], embedding_model: TextEmbedding) -> list[tuple[dict[str, Any], Path]]:
+def add_embeddings_to_source(batch: list[SourceWithEmbeddingText], embedding_model: TextEmbedding, embedding_field_name = 'emb') -> list[tuple[dict[str, Any], Path]]:
+    """
+    Given a batch of `SourceWithEmbeddingText`, calculates the embeddings and returns the documents with the embeddings (integrated).
+
+    :param batch: a batch of source documents with their embedding texts.
+    :param embedding_model: the model to be used for embedding.
+    :param embedding_field_name: name of the embedding field in the source document.
+    :return: a tuple of source documents with embeddings (0) and the original file name (1).
+    """
     embeddings = list(embedding_model.embed(list(map(lambda ele: ele[1], batch))))
     src_emb = zip(list(map(lambda ele: ele[0], batch)), embeddings, list(map(lambda ele: ele[2], batch)))
-    return list(map(lambda ele: ({**ele[0], 'emb': ele[1].tolist()}, ele[2]), src_emb))
+    return list(map(lambda ele: ({**ele[0], embedding_field_name: ele[1].tolist()}, ele[2]), src_emb))
 
 
 def preprocess_batch(batch: list[dict[str, Any]], index_name: str) -> list[dict[str, Any]]:
-    return list(map(lambda ele: {"_op_type": "index", "_index": index_name, "_source": ele}, batch))
+    """
+    Given a list of source documents, builds the structure for OpenSearch.
+
+    :param batch: batch of source documents.
+    :param index_name: name of the OpenSearch index.
+    :return: a list of prepared documents for import.
+    """
+    return list(map(lambda ele: {'_op_type': 'index', '_index': index_name, '_source': ele}, batch))
