dab895410b6311cc1761de6b8bbf2b720d18afda
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-10-01 13:45:35 +0200

    refactor (import data): mimick harvesting event

--- scripts/postgres_data/import_data.py
+++ scripts/postgres_data/import_data.py
@@ -5,13 +5,16 @@ import pgsql
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from lxml import etree as ET
 
 load_dotenv()
 
 USER = os.environ.get('POSTGRES_ADMIN')
 PW = os.environ.get('POSTGRES_PASSWORD')
 
-files: list[Path] = (list(Path('data').rglob("*.xml")))
+NS = {"oai": "http://www.openarchives.org/OAI/2.0/"}
+
+files: list[Path] = (list(Path('data').rglob("harvests_DANS_arch/*.xml")))
 
 with pgsql.Connection(('127.0.0.1', 5432), USER, PW, tls = False) as db:
     print(db)
@@ -22,11 +25,20 @@ with pgsql.Connection(('127.0.0.1', 5432), USER, PW, tls = False) as db:
             with open(file) as f:
                 xml = f.read()
 
-            print(file)
+            existing_tree = ET.parse(file)
+            existing_root = existing_tree.getroot()
+            identifier = existing_root.find("./oai:header/oai:identifier", namespaces=NS)
+
+            if identifier is None:
+                raise ValueError(f'XML OAI-PMH record {file} without identifier')
+
+            print(identifier.text)
 
             # escape single quotes in XML content: https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-STRINGS
             db.execute(f"""
-            INSERT INTO raw (info)VALUES ( XMLPARSE(DOCUMENT '{xml.replace("'", "''")}'));
+            INSERT INTO harvest_events (record_identifier, raw_metadata, repository_id, endpoint_id, action, metadata_protocol) VALUES ( '{identifier.text}', XMLPARSE(DOCUMENT '{xml.replace("'", "''")}'), (SELECT id from repositories WHERE code='DANS'), (SELECT id from endpoints WHERE harvest_url='https://easy.dans.knaw.nl/oai'), 'create', 'OAI-PMH' );
             """)
+
+            #break
     except Exception as e:
         print(f'An error occurred when loading data in DB: {e}', file=sys.stderr)
