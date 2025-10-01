67955049a9ed4b05ade3c0a510148a09bec4798e
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-10-01 07:55:40 +0200

    refactor (DB schema): load SQL statements in defined order

--- scripts/postgres_data/create_db.py
+++ scripts/postgres_data/create_db.py
@@ -12,15 +12,18 @@ load_dotenv()
 USER = os.environ.get('POSTGRES_ADMIN')
 PW = os.environ.get('POSTGRES_PASSWORD')
 
+sql_files = ['types.sql', 'tables.sql', 'indexes.sql']
+
 with pgsql.Connection(('127.0.0.1', 5432), USER, PW, tls = False) as db:
 
-    try:
-        db.execute("""
-        DROP TABLE raw;
-        CREATE TABLE raw( id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, info XML);
-        """)
-    except Exception as e:
-        print(f'An error occurred when creating DB: {e}', file=sys.stderr)
+    for sql_f in sql_files:
+        try:
+            with open(f'create_sql/{sql_f}') as f:
+                sql_statements = f.read()
+            res = db.execute(sql_statements)
+            print(f'Executed {sql_f}')
+        except Exception as e:
+            print(f'An error occurred when creating DB: {e}', file=sys.stderr)
 
 
 
