65c75077b4a394a02ade209ab065bb0e1700bba8
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-10-03 08:30:27 +0200

    refactor (import postgres): import data from different endpoints

--- scripts/postgres_data/create_sql/seed.sql
+++ scripts/postgres_data/create_sql/seed.sql
@@ -11,15 +11,106 @@ VALUES
     ('HAL Science', 'HAL', 'French open archive', 'https://hal.science', true)
 ON CONFLICT (code) DO NOTHING;
 
--- Sample endpoint
+-- DANS Archaeology
 INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
 SELECT
     r.id,
-    'DANS Easy Dataset Archive',
-    'https://easy.dans.knaw.nl/oai',
+    'Archaeology Data Station',
+    'https://archaeology.datastations.nl/oai',
     'OAI-PMH',
     'Multidisciplinary',
     true
 FROM repositories r
 WHERE r.code = 'DANS'
+ON CONFLICT (name) DO NOTHING;
+
+-- DANS Social Sciences Data Station
+INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
+SELECT
+    r.id,
+    'Social Sciences Data Station',
+    'https://ssh.datastations.nl/oai',
+    'OAI-PMH',
+    'Multidisciplinary',
+    true
+FROM repositories r
+WHERE r.code = 'DANS'
+ON CONFLICT (name) DO NOTHING;
+
+-- DANS Life Sciences
+INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
+SELECT
+    r.id,
+    'Life Sciences',
+    'https://lifesciences.datastations.nl/oai',
+    'OAI-PMH',
+    'Multidisciplinary',
+    true
+FROM repositories r
+WHERE r.code = 'DANS'
+ON CONFLICT (name) DO NOTHING;
+
+-- DANS Physical and Technical Sciences
+INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
+SELECT
+    r.id,
+    'Physical and Technical Sciences',
+    'https://phys-techsciences.datastations.nl/oai',
+    'OAI-PMH',
+    'Multidisciplinary',
+    true
+FROM repositories r
+WHERE r.code = 'DANS'
+ON CONFLICT (name) DO NOTHING;
+
+-- DANS Generalist
+INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
+SELECT
+    r.id,
+    'Generalist',
+    'https://dataverse.nl/oai',
+    'OAI-PMH',
+    'Multidisciplinary',
+    true
+FROM repositories r
+WHERE r.code = 'DANS'
+ON CONFLICT (name) DO NOTHING;
+
+-- SwissUbase
+INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
+SELECT
+    r.id,
+    'SwissUbase',
+    'https://www.swissubase.ch/oai-pmh/v1/oai',
+    'OAI-PMH',
+    'Multidisciplinary',
+    true
+FROM repositories r
+WHERE r.code = 'SWISS'
+ON CONFLICT (name) DO NOTHING;
+
+-- DABAR
+INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
+SELECT
+    r.id,
+    'DABAR',
+    'https://dabar.srce.hr/oai',
+    'OAI-PMH',
+    'Multidisciplinary',
+    true
+FROM repositories r
+WHERE r.code = 'DABAR'
+ON CONFLICT (name) DO NOTHING;
+
+-- HAL
+INSERT INTO endpoints (repository_id, name, harvest_url, protocol, scientific_discipline, is_active)
+SELECT
+    r.id,
+    'HAL',
+    'https://api.archives-ouvertes.fr/oai/hal',
+    'OAI-PMH',
+    'Multidisciplinary',
+    true
+FROM repositories r
+WHERE r.code = 'HAL'
 ON CONFLICT (name) DO NOTHING;
--- scripts/postgres_data/import_data.py
+++ scripts/postgres_data/import_data.py
@@ -14,31 +14,67 @@ PW = os.environ.get('POSTGRES_PASSWORD')
 
 NS = {"oai": "http://www.openarchives.org/OAI/2.0/"}
 
-files: list[Path] = (list(Path('data').rglob("*.xml")))
+def import_data(repo_code: str, harvest_url: str, dir: Path):
+    files: list[Path] = list(dir)
 
-with pgsql.Connection(('127.0.0.1', 5432), USER, PW, tls = False) as db:
-    print(db)
+    with pgsql.Connection(('127.0.0.1', 5432), USER, PW, tls = False) as db:
+        try:
+            for file in files:
 
+                with open(file) as f:
+                    xml = f.read()
+
+                existing_tree = ET.parse(file)
+                existing_root = existing_tree.getroot()
+                identifier = existing_root.find("./oai:header/oai:identifier", namespaces=NS)
+
+                if identifier is None:
+                    raise ValueError(f'XML OAI-PMH record {file} without identifier')
+
+                print(identifier.text)
+
+                # escape single quotes in XML content: https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-STRINGS
+                db.execute(f"""
+                INSERT INTO harvest_events 
+                    (record_identifier, 
+                    raw_metadata, 
+                    repository_id, 
+                    endpoint_id, 
+                    action, 
+                    metadata_protocol,
+                    metadata_format
+                    ) 
+                VALUES ( 
+                    '{identifier.text}', 
+                    XMLPARSE(DOCUMENT '{xml.replace("'", "''")}'), 
+                    (SELECT id from repositories WHERE code='{repo_code}'),
+                     (SELECT id from endpoints WHERE harvest_url='{harvest_url}'), 
+                     'create', 
+                     'OAI-PMH',
+                      'XML');
+                """)
+
+                #break
+        except Exception as e:
+            print(f'An error occurred when loading data in DB: {e}', file=sys.stderr)
+
+
+with pgsql.Connection(('127.0.0.1', 5432), USER, PW, tls=False) as db:
     try:
-        for file in files:
-
-            with open(file) as f:
-                xml = f.read()
-
-            existing_tree = ET.parse(file)
-            existing_root = existing_tree.getroot()
-            identifier = existing_root.find("./oai:header/oai:identifier", namespaces=NS)
-
-            if identifier is None:
-                raise ValueError(f'XML OAI-PMH record {file} without identifier')
-
-            print(identifier.text)
-
-            # escape single quotes in XML content: https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-STRINGS
-            db.execute(f"""
-            INSERT INTO harvest_events (record_identifier, raw_metadata, repository_id, endpoint_id, action, metadata_protocol) VALUES ( '{identifier.text}', XMLPARSE(DOCUMENT '{xml.replace("'", "''")}'), (SELECT id from repositories WHERE code='DANS'), (SELECT id from endpoints WHERE harvest_url='https://easy.dans.knaw.nl/oai'), 'create', 'OAI-PMH' );
-            """)
-
-            #break
+        res = db.execute(f"""
+        TRUNCATE TABLE records;
+        TRUNCATE TABLE harvest_events;
+        """)
     except Exception as e:
-        print(f'An error occurred when loading data in DB: {e}', file=sys.stderr)
+        print(f'An error occurred when deleting data in DB: {e}', file=sys.stderr)
+
+
+import_data('DANS', 'https://archaeology.datastations.nl/oai', Path('data/harvests_DANS_arch').rglob("*.xml"))
+import_data('DANS', 'https://ssh.datastations.nl/oai', Path('data/harvests_DANS_soc').rglob("*.xml"))
+import_data('DANS', 'https://lifesciences.datastations.nl/oai', Path('data/harvests_DANS_life').rglob("*.xml"))
+import_data('DANS', 'https://phys-techsciences.datastations.nl/oai', Path('data/harvests_DANS_phystech').rglob("*.xml"))
+import_data('DANS', 'https://dataverse.nl/oai', Path('data/harvests_DANS_gen').rglob("*.xml"))
+import_data('SWISS', 'https://www.swissubase.ch/oai-pmh/v1/oai', Path('data/harvests_SWISS_dc_datacite').rglob("*.xml"))
+import_data('DABAR', 'https://dabar.srce.hr/oai', Path('data/harvests_DABAR').rglob("*.xml"))
+import_data('HAL', 'https://api.archives-ouvertes.fr/oai/hal', Path('data/harvests_HAL_sample').rglob("*.xml"))
+
