a7fe4e9c58c0176d29f2a962f761bd7bceeac22e
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-10-17 14:40:31 +0200

    refactor (psycopg): use new client in create_db.py

--- scripts/postgres_data/create_db.py
+++ scripts/postgres_data/create_db.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env -S uv run --script
 import sys
-
-import pgsql
 import os
 from dotenv import load_dotenv
+import psycopg
+import traceback
 
 load_dotenv()
 
-#print(os.environ)
+# print(os.environ)
 
 USER = os.environ.get('POSTGRES_ADMIN')
 PW = os.environ.get('POSTGRES_PASSWORD')
@@ -17,19 +17,21 @@ PORT = os.environ.get('POSTGRES_PORT')
 if not USER or not PW:
     raise ValueError('Missing POSTGRES_ADMIN or POSTGRES_PASSWORD in environment.')
 
+sql_files = ['types.sql', 'tables.sql', 'indexes.sql', 'triggers.sql', 'seed.sql', 'views.sql', 'permissions.sql',
+             'verify.sql']
 
-sql_files = ['types.sql', 'tables.sql', 'indexes.sql', 'triggers.sql', 'seed.sql', 'views.sql', 'permissions.sql', 'verify.sql']
-
-with pgsql.Connection((ADDRESS if ADDRESS else '127.0.0.1', int(PORT) if PORT else 5432), USER, PW, tls = False) as db:
-
-    for sql_f in sql_files:
-        try:
-            with open(f'create_sql/{sql_f}') as f:
-                sql_statements = f.read()
-            res = db.execute(sql_statements)
-            print(f'Executed {sql_f}')
-        except Exception as e:
-            print(f'An error occurred when creating DB with {sql_f}: {e}', file=sys.stderr)
-
-
-
+try:
+    with psycopg.connect(dbname='admin', user=USER, host=ADDRESS if ADDRESS else '127.0.0.1', password=PW,
+                         port=int(PORT) if PORT else 5432) as conn:
+        cur = conn.cursor()
+        for sql_f in sql_files:
+            try:
+                with open(f'create_sql/{sql_f}') as f:
+                    sql_statements = f.read()
+                res = cur.execute(sql_statements)
+                print(f'Executed {sql_f}')
+            except Exception as e:
+                print(f'An error occurred when creating DB with {sql_f}: {e}', file=sys.stderr)
+except Exception as e:
+    print(f'An error occurred when loading data in DB: {e}', file=sys.stderr)
+    traceback.print_exc(file=sys.stderr)
