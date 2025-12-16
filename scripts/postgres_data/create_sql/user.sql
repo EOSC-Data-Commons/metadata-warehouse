edd10e3c0f9fdda5aa48bb679e3d8530d7589c5b
Chuan Ping Lee <lecping@yahoo.com>
2025-12-16 15:33:31 +0100

    feat(datamodel): add username to metadata warehouse

---
+++ scripts/postgres_data/create_sql/user.sql
@@ -0,0 +1,15 @@
+---------------------------------------------------------
+-- CREATE GOLOBAL USERS
+
+CREATE ROLE reggie LOGIN PASSWORD 'reggie';
+CREATE ROLE jusong LOGIN PASSWORD 'jusong';
+CREATE ROLE ritwik LOGIN PASSWORD 'ritwik';
+CREATE ROLE tobias LOGIN PASSWORD 'tobias';
+CREATE ROLE josip LOGIN PASSWORD 'josip';
+CREATE ROLE michal LOGIN PASSWORD 'michal';
+CREATE ROLE ping LOGIN PASSWORD 'ping';
+CREATE ROLE vincent LOGIN PASSWORD 'vincent';
+CREATE ROLE eko LOGIN PASSWORD 'eko';
+
+-- System service accounts
+CREATE ROLE script LOGIN PASSWORD 'script';
