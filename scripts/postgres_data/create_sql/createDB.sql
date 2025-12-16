e5f04dc6991adb72b579160b6ba8439e06f18eaa
Chuan Ping Lee <lecping@yahoo.com>
2025-12-16 15:36:20 +0100

    feat(datamodel): add four databases for metadata warehouse

---
+++ scripts/postgres_data/create_sql/createDB.sql
@@ -0,0 +1,8 @@
+-- database to store metadata about datasets
+CREATE DATABASE datasetDB;
+-- database to store metadata about file registry from datasets
+CREATE DATABASE fileDB;
+-- database to store metadata about tool registry
+CREATE DATABASE toolDB;
+-- database to store metadata about user usage and query
+CREATE DATABASE userDB;
