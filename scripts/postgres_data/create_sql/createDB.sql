01d85de6f08ecf75821102d75a4951ef4371ef7c
Vincent Emonet <vincent.emonet@gmail.com>
2026-01-14 10:15:25 +0100

    refactor: rename `userDB` to `appDB`

--- scripts/postgres_data/create_sql/createDB.sql
+++ scripts/postgres_data/create_sql/createDB.sql
@@ -5,4 +5,4 @@ CREATE DATABASE fileDB;
 -- database to store metadata about tool registry
 CREATE DATABASE toolDB;
 -- database to store metadata about user usage and query
-CREATE DATABASE userDB;
+CREATE DATABASE appDB;
