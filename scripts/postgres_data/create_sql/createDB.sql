8a579480b92ca104d12f89a7169de47e93c3a562
Chuan Ping Lee <lecping@yahoo.com>
2026-02-12 06:36:26 +0100

    fix(datamodel): initialize database with postgres superuser and default template

--- scripts/postgres_data/create_sql/createDB.sql
+++ scripts/postgres_data/create_sql/createDB.sql
@@ -1,8 +1,28 @@
 -- database to store metadata about datasets
-CREATE DATABASE datasetDB;
+CREATE DATABASE datasetdb
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TEMPLATE = template0;
+
 -- database to store metadata about file registry from datasets
-CREATE DATABASE fileDB;
+CREATE DATABASE filedb
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TEMPLATE = template0;
+
 -- database to store metadata about tool registry
-CREATE DATABASE toolDB;
+CREATE DATABASE tooldb
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TEMPLATE = template0;
+
 -- database to store metadata about user usage and query
-CREATE DATABASE appDB;
+CREATE DATABASE appdb
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TEMPLATE = template0;
+
+-- run this command to apply RBAC template to all databases
+-- for db in datasetdb filedb tooldb appdb; do
+-- sed "s/{{DB_NAME}}/$db/g" rbac_template.sql | psql -U postgres -d $db
+-- done
