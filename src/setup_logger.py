bcd7380e16f3e5a7222e4dd9fdcec7bbdb27169b
Tobias Schweizer <tobias.schweizer@switch.ch>
2026-06-02 10:03:24 +0200

    refactor (setup logger): centralize logger setup

--- src/setup_logger.py
+++ src/setup_logger.py
@@ -4,4 +4,4 @@ from logging.config import dictConfig
 from config.logging_config import LOGGING_CONFIG
 
 dictConfig(LOGGING_CONFIG)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('fast_api')
