009c0e625a63ef66008469a29d6217b901399987
mkranjcev <kranjcev@srce.hr>
2026-06-16 14:37:01 +0200

    chore(ruff): reformat new files

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,10 @@
+import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -14,8 +13,8 @@ load_dotenv()
 config = context.config
 
 config.set_main_option(
-    "sqlalchemy.url",
-    f"postgresql+psycopg2://{os.environ['POSTGRES_ADMIN']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ.get('POSTGRES_ADDRESS', '127.0.0.1')}:{os.environ.get('POSTGRES_PORT', 5432)}/datasetdb"
+    'sqlalchemy.url',
+    f'postgresql+psycopg2://{os.environ["POSTGRES_ADMIN"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ.get("POSTGRES_ADDRESS", "127.0.0.1")}:{os.environ.get("POSTGRES_PORT", 5432)}/datasetdb',
 )
 
 
@@ -48,12 +47,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option('sqlalchemy.url')
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        dialect_opts={'paramstyle': 'named'},
     )
 
     with context.begin_transaction():
@@ -69,14 +68,12 @@ def run_migrations_online() -> None:
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+        prefix='sqlalchemy.',
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
