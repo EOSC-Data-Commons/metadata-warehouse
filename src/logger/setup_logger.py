73c5a84d452d64932dc4e5f25fec3895ef0e9c86
Tobias Schweizer <tobias.schweizer@switch.ch>
2026-07-31 10:22:27 +0200

    fix(formatting): fix format

--- src/logger/setup_logger.py
+++ src/logger/setup_logger.py
@@ -3,10 +3,11 @@ from logging.config import dictConfig
 from config.logging_config import LOGGING_CONFIG
 
 
-def setup_logging()-> None:
+def setup_logging() -> None:
     try:
         dictConfig(LOGGING_CONFIG)
     except Exception as e:
         import traceback
+
         traceback.print_exc()
         raise
