e966b35738915c63a0ec1559ed77263aac17bd4b
Tobias Schweizer <tobias.schweizer@switch.ch>
2026-07-31 10:31:06 +0200

    refactor(logger): remove type annotation (wrong)

--- src/logger/setup_logger.py
+++ src/logger/setup_logger.py
@@ -3,7 +3,7 @@ from logging.config import dictConfig
 from config.logging_config import LOGGING_CONFIG
 
 
-def setup_logging() -> None:
+def setup_logging():
     try:
         dictConfig(LOGGING_CONFIG)
     except Exception as e:
