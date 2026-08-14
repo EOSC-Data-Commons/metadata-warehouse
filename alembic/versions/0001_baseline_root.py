ee78fb66748bedabd90e03d6b250d464c2855d60
mkranjcev <kranjcev@srce.hr>
2026-08-14 11:55:15 +0200

    chore(ruff): fix indents

--- alembic/versions/0001_baseline_root.py
+++ alembic/versions/0001_baseline_root.py
@@ -8,9 +8,9 @@ Create Date: 2026-08-14 09:55:11.483473
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '0001_baseline'
