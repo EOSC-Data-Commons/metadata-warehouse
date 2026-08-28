eb0f7dc5f78342379a9f5b2e1d5870f3a6ba5847
Tobias Schweizer <tobias.schweizer@switch.ch>
2026-08-28 16:06:09 +0200

    feat(deduplication): add deduplication job type (ongoing)

--- src/transform/tasks.py
+++ src/transform/tasks.py
@@ -1,5 +1,6 @@
 from transform.celery_app_def import celery_app
+from transform.deduplication_task import find_duplicates, remove_duplicates
 from transform.file_meta_task import add_file_metadata
 from transform.transform_task import transform_batch
 
-__all__ = ['celery_app', 'add_file_metadata', 'transform_batch']
+__all__ = ['celery_app', 'add_file_metadata', 'transform_batch', 'find_duplicates', 'remove_duplicates']
