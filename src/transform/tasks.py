296efe5956eb41c37259cffdb0e73e2e45f7d1a7
Tobias Schweizer <tobias.schweizer@switch.ch>
2026-08-28 09:47:07 +0200

    fix(formatting): fix formatting

--- src/transform/tasks.py
+++ src/transform/tasks.py
@@ -2,4 +2,4 @@ from transform.celery_app_def import celery_app
 from transform.file_meta_task import add_file_metadata
 from transform.transform_task import transform_batch
 
-__all__ = ["celery_app", "add_file_metadata", "transform_batch"]
+__all__ = ['celery_app', 'add_file_metadata', 'transform_batch']
